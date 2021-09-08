1ec5c180053d91dad602c73565e4eb3ba0a88655
ni30gupta <nguptani30@gmail.com>
2021-09-08 12:49:30 +0530

    done

--- vote/index.php
+++ vote/index.php
@@ -1,13 +1,15 @@
 <?php
-$con = mysqli_connect('localhost', 'root', 'root', '28aug');
+$con = mysqli_connect('localhost', 'root', '', '28aug');
 
 $res = mysqli_query($con, "SELECT * from vote");
 if (isset($_POST['vote'])) {
      $id = $_POST['id'];
      $vote = $_POST['vote'];
+     $finalMsg = "msg$id";
 
      if (isset($_COOKIE["vote$id"])) {
-          $msg = "Already Voted";
+
+          $$finalMsg = "Already Voted";
      } else {
 
 
@@ -20,30 +22,34 @@ if (isset($_POST['vote'])) {
                $updateSql = "UPDATE vote SET vote2_count = vote2_count+1 WHERE id='$id'";
           }
           mysqli_query($con, $updateSql);
-          $msg = "Thanks for voting";
           setcookie("vote$id", $id, time() + 10);
+          $$finalMsg = "Thanks for voting";
      }
 }
+// echo $msg1;
+// echo $msg2;
 
 
 while ($rows = mysqli_fetch_assoc($res)) {
      $id  = $rows['id'];
 
 
-
-
 ?>
 
      <form method="post">
-          <p>
-               <?php
-               if (isset($msg)) {
-                    echo $msg;
-                    $msg = "";
-               }
-               ?>
-          </p>
+
           <input type="submit" name="vote" value="<?php echo $rows['vote1']; ?>"> <span>(<?php echo $rows['vote1_count']; ?>)</span> <input name="id" value="<?php echo $id; ?>" hidden type="text"> V/s
+
           <input type="submit" name="vote" value="<?php echo $rows['vote2']; ?>"> <span>(<?php echo $rows['vote2_count']; ?>)</span> <input name="id" value="<?php echo $id; ?>" hidden type="text"> <br> <br>
+          <?php
+          if (isset($msg1) && $msg1 != "") {
+               echo $msg1;
+               $msg1 = "";
+          }
+          if (isset($msg2) && $msg2 != "") {
+               echo $msg2;
+               $msg2 = "";
+          }
+          ?>
      </form>
 <?php } ?>
